ba1155514d993ee4521c3e5094d660fceeb20874
Wilson Ejulu <emox1995@gmail.com>
2024-08-12 17:10:46 +0300

    alignment ok

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,44 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import Todo from "./Todo";
+import { useState } from "react";
 
 export default function Board() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  function handleChange(event) {
+    const todoUpdate = event.target.value;
+    setTodo(todoUpdate);
+    console.log(todoUpdate);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setTodos((prevTodos) => {
+      return [...prevTodos, todo];
+    });
+    setTodo("");
+  }
+
   return (
     <div className="flex flex-col h-5/6 w-1/2 rounded-xl border p-4 bg-yellow-600  ">
-      <div className="flex mt-3 gap-3">
-        <Input placeholder="Enter todo " className=" ml-2 "></Input>
+      <form onSubmit={handleSubmit} className="flex mt-3 gap-3">
+        <Input
+          name="todoInput"
+          placeholder="Enter todo "
+          className=" ml-2 "
+          onChange={handleChange}
+          value={todo}
+        ></Input>
+
         <Button className="mr-2">Add</Button>
-      </div>
-      <div className=" bg-white mt-3 flex-grow  lg:flex lg:items-start lg:flex-wrap lg:justify-center overflow-auto">
-        <Todo/>
-        <Todo/>
-        <Todo/>
-        <Todo/>
-        <Todo/>
-        <Todo/>
-        <Todo/>
-        <Todo/>
+      </form>
+      <div className=" bg-white overflow-auto content-start mt-3 flex-grow justify-around lg:flex lg:flex-wrap">
+        {todos.map((item, index) => (
+          <Todo key={index} todo={item} />
+        ))}
       </div>
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,10 @@
 import { Checkbox } from "@/components/ui/checkbox";
 
-export default function Todo() {
+export default function Todo(props) {
   return (
-    <div className="flex items-center gap-1 mx-2 py-3 mt-3 lg:w-[45%]">
+    <div className="flex flex-grow items-center  gap-1 mx-2 py-3 my-3 lg:w-[45%]">
       <Checkbox className="ml-1" />
-      <p> Wake up</p>
+      <p> {props.todo}</p>
     </div>
   );
 }
